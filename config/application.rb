9609053a818f80de64a60fa5e06b66321b7f412a
Yusuke_Saito <ysaito8015@gmail.com>
2014-06-23 01:24:38 +0900

    6-1 visual design

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,16 @@ Bundler.require(*Rails.groups)
 
 module Baukis
   class Application < Rails::Application
+      config.time_zone = 'Tokyo'
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+      config.i18n.default_locale = :ja
+      config.generators do |g|
+        g.helper false
+        g.assets false
+        g.test_framework :rspec
+        g.controller_specs false
+        g.view_specs false
+      end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
