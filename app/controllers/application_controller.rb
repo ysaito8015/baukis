1ebb14f9c1045c2a41eeebcd26769d2ef1d97fcb
Yusuke_Saito <ysaito8015@gmail.com>
2014-07-10 01:31:59 +0900

    staffmember追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+  
+  include ErrorHandlers if Rails.env.production?
+
   private
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
